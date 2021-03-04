a4293a69ea499eab62d7ff9c2e6266b761c66293
Hackerry <j4niu@ucsd.edu>
2021-03-04 14:00:39 -0800

    Change all activities page style and fix minor issues

--- routes/report.js
+++ routes/report.js
@@ -1,4 +1,8 @@
 const activityUtil = require("../public/js/activities.js");
+const userFile = "users.json";
+const fs = require('fs');
+
+const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 /*
  * GET report page.
@@ -8,9 +12,12 @@ exports.view = function(req, res){
     var username = req.cookies.username;
 
     var data = {};
+    
+    var users = JSON.parse(fs.readFileSync(userFile, 'utf8'));
+    var startDay = users[username]['settings']['startDay'];
 
-    // Remove types that have no hours
     data['data'] = activityUtil.getActivitySummary(username);
+    data['startDay'] = dayName[startDay];
 
     console.log("Data gathered:", data['data']);
 
